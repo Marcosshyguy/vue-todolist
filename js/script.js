0b8e8257107efb4049d7403401a0548e572af42d
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-27 16:08:15 +0200

    add v-for to the li elemen that multiply when i add  some value inside  to-do input

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const {createApp} = Vue
 createApp({
     data: function(){
         return {
-            
+           toDoList :[],
+           userChoice: ""
+        }
+    },
+    methods :{
+        // push the user choice at the beginning
+        pushUserChoice : function(){
+            this.toDoList.push(this.userChoice);
         }
     }
 }).mount("#app")
