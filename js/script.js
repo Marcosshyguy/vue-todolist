3b3acedf33906ca274d4ea750aa8f04f4be72ff6
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-28 13:11:35 +0200

    lastn edit

--- js/script.js
+++ js/script.js
@@ -14,18 +14,18 @@ createApp({
         }
     },
     methods :{
-        // push the user choice at the beginning
+        // push the user choice at the beginning of the array
         pushUserChoice : function(){
-            if(this.userChoice.toDo){
+            if(this.userChoice.toDo && this.userChoice.toDo.length > 5){
                 this.toDoList.unshift({...this.userChoice});
                 // this.userChoice.toDo = "";
                 this.userChoice.toDo = "";
             }
         },
-        // isItDone : function(Index){
-        //     // this.indexPosition = Index;
-        //     // this.isDone = !this.isDone;
-        // },
+        // function that switch the boolean value and it work with the class Underline in gtml
+        isItDone : function(index){
+            this.toDoList[index].isDone = !this.toDoList[index].isDone;
+        },
         removeTask: function(indexs){
             this.toDoList.splice(indexs,1);
         }
